a64d031718dca4568dd83a397778eb9870feeacd
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-26 19:24:57 +0300

    timline page

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,44 +1,125 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import NavBar from "./../NavBar";
 import Card from "./../Card";
-import "./style.css";
 
 const BASE_URL = "http://localhost:5000";
 
 const Timeline = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
   const [postCards, setPostCards] = useState([]);
+  const [pageLoader, setPageLoader] = useState(true);
 
   useEffect(() => {
+    getUserDetails();
     getUserFollowing();
     // eslint-disable-next-line
   }, []);
 
-  const getPosts = async (following) => {
+  const getUserDetails = async () => {
     const userStorage = localStorage.getItem("user");
     const userData = JSON.parse(userStorage);
-    const res = await axios.put(`${BASE_URL}/posts/followingPosts/${userData.id}`, {
-      userFollowing: following
-    });
-    setPostCards(res.data);
+    if (userData) {
+      const res = await axios.get(`${BASE_URL}/users/${userData.id}`);
+      setUser(res.data);
+    }
   };
 
   const getUserFollowing = async () => {
     const userStorage = localStorage.getItem("user");
     const userData = JSON.parse(userStorage);
     const res = await axios.get(`${BASE_URL}/users/${userData.id}`);
-    getPosts(res.data.following);
+    getPosts(userData.id, res.data.following);
+  };
+
+  const getPosts = async (userId, followingList) => {
+    const res = await axios.put(`${BASE_URL}/posts/followingPosts/${userId}`, {
+      userFollowing: followingList,
+    });
+    setPostCards(res.data);
+    setPageLoader(false);
   };
 
   return (
-    <div>
-      <NavBar />
-      <div className="cards">
-      {postCards.map((card) => {
-        return <Card card={card} key={card._id}/>;
-      })}
-    </div>
-    </div>
+    <>
+      {user ? (
+        <>
+          <NavBar user={user} />
+          <div className="cards">
+            {postCards.map((card) => {
+              if (user.likes.find((post) => post._id === card._id)) {
+                if (card.creator._id === user._id)
+                  return (
+                    <Card
+                      card={card}
+                      user={user}
+                      likeStatus={true}
+                      deleteStatus={true}
+                      render={getUserFollowing}
+                      key={card._id}
+                    />
+                  );
+                else
+                  return (
+                    <Card
+                      card={card}
+                      user={user}
+                      likeStatus={true}
+                      deleteStatus={false}
+                      render={getUserFollowing}
+                      key={card._id}
+                    />
+                  );
+              } else {
+                if (card.creator._id === user._id)
+                  return (
+                    <Card
+                      card={card}
+                      user={user}
+                      likeStatus={false}
+                      deleteStatus={true}
+                      render={getUserFollowing}
+                      key={card._id}
+                    />
+                  );
+                else
+                  return (
+                    <Card
+                      card={card}
+                      user={user}
+                      likeStatus={false}
+                      deleteStatus={false}
+                      render={getUserFollowing}
+                      key={card._id}
+                    />
+                  );
+              }
+            })}
+          </div>
+        </>
+      ) : pageLoader ? (
+        <div className="centerWrapper">
+          <div className="lds-ring">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      ) : (
+        <div className="centerWrapper">
+          <div className="signupLoginTitle">
+            <h1>YOU HAVE TO LOGIN FIRST</h1>
+          </div>
+          <div className="signupLoginButtons">
+            <button onClick={() => navigate("/login")}>Login</button>
+            <button onClick={() => navigate("/signup")}>Signup</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
